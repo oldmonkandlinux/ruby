09955f6069ee663c88da2492b2d1177939539943
root <root@ansible-client.example.com>
2020-02-27 08:51:41 +0530

    mass

---
+++ arithmetic.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+puts 32+19
+puts 32-19
+puts 2*3
+puts 2**3
+puts 8/2
+puts 8.0/2
+
+puts 8.0/0
+
+puts 'done'
---
+++ strings.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+puts "hello".length
+puts "hello".reverse
+
+puts "Jar " * 2
+
+puts "2".to_s * 3
+puts 2.to_s * 3
+puts 2.to_i * 3
